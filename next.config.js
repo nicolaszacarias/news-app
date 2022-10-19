816525633ed3d19a65e9e03d087e25ff3a58b8c9
nicolaszacaria <zacariasnicolas95@gmail.com>
2022-10-19 10:31:37 -0300

    nuevo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: { 
+    domains: ["www.gannett-cdn.com", "media.cnn.com", "www.reuters.com", "assets2.cbsnewsstatic.com", "www.apple.com", "images.macrumors.com", "s.abcnews.com", "cdn.vox-cdn.com", "hips.hearstapps.com", "profootballtalk.nbcsports.com", "a3.espncdn.com", "kubrick.htvapps.com", "www.washingtonpost.com", "ichef.bbci.co.uk", "assets-prd.ignimgs.com", "img.huffingtonpost.com"],
+  },
   swcMinify: true,
 }
 
---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import PageLayout from "./components/pageLayout"
+
+export default function About() {
+    return <PageLayout title="About">
+    <h1>Holas</h1>
+    </PageLayout>
+}
